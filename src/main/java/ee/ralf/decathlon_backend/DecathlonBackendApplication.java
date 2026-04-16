838ad618d4150401297a7a85190163ecc7da2cee
Ralf Soiela <soiela@tlu.ee>
2026-04-17 00:31:54 +0300

    Initial commit

---
+++ src/main/java/ee/ralf/decathlon_backend/DecathlonBackendApplication.java
@@ -0,0 +1,13 @@
+package ee.ralf.decathlon_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecathlonBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecathlonBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ee/ralf/decathlon_backend/DecathlonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.ralf.decathlon_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecathlonBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
